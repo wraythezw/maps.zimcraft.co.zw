f659598e7bf5192f641dfcc6efb45b9341304ae8
BuildTools <unconfigured@null.spigotmc.org>
2021-12-26 02:00:36 +0200

    Map Update

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1639872033",
+        "cacheTag": "1640476834",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
